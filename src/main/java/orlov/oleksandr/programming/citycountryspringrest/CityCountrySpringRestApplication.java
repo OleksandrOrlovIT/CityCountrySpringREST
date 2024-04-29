27a16f97ca1c91993f5bb2e1f2f774f501967624
Oleksandr Orlov <alexanderorlovkh@gmail.com>
2024-04-29 18:00:16 +0300

    Init commit

---
+++ src/main/java/orlov/oleksandr/programming/citycountryspringrest/CityCountrySpringRestApplication.java
@@ -0,0 +1,13 @@
+package orlov.oleksandr.programming.citycountryspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityCountrySpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityCountrySpringRestApplication.class, args);
+    }
+
+}
