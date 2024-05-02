e4f6c52c4bde83fe8e08a12e78a3ce5b54584088
Oleksandr Orlov <alexanderorlovkh@gmail.com>
2024-05-02 18:32:48 +0300

    Changed app configuration to use docker compose to run postgres. Added test configuration. Created tests for CountryController class.

--- src/main/java/orlov/oleksandr/programming/citycountryspringrest/exception/GlobalExceptionHandler.java
+++ src/main/java/orlov/oleksandr/programming/citycountryspringrest/exception/GlobalExceptionHandler.java
@@ -4,41 +4,88 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NullPointerException.class)
-    public ResponseEntity<String> handleNullPointerException(NullPointerException ex) {
+    public ResponseEntity<?> handleNullPointerException(NullPointerException ex) {
         logException("NullPointer Exception occurred", ex);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "BAD_REQUEST");
+        response.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        logException(ex.getMessage(), ex);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "BAD_REQUEST");
+
+        List<String> errors = new ArrayList<>();
+        for(var error : ex.getBindingResult().getAllErrors()) {
+            errors.add(error.getDefaultMessage());
+        }
+
+        response.put("errors", errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException ex) {
         logException("EntityNotFoundException occurred", ex);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "NOT_FOUND");
+        response.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException ex) {
         logException("IllegalArgumentException occurred", ex);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "BAD_REQUEST");
+        response.put("errorMessage", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
+    public ResponseEntity<?> handleException(Exception ex) {
         logException("Exception occurred", ex);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "INTERNAL_SERVER_ERROR");
+        response.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+    public ResponseEntity<?> handleRuntimeException(RuntimeException ex) {
         logException("RuntimeException occurred", ex);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "INTERNAL_SERVER_ERROR");
+        response.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 
     private void logException(String message, Exception ex) {
